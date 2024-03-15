2700641699a36aab7eecbbc57098f3c9f18b5bf6
José Antonio Calvo <ciberjaci7@gmail.com>
2024-03-15 10:28:55 +0100

    Cambios JS

--- main.js
+++ main.js
@@ -4,8 +4,29 @@ const listContacts= async()=>{
     try {
         const response= await fetch('API.php')
         
-        const data = await response.json();
-        console.log(data);
+        const users = await response.json();
+        console.log(users);
+        
+        let contenido=``
+
+        users.forEach((user, index) => {
+            
+            contenido+=`
+            
+            <tr>
+                <td>${index+1}</td>
+                <td>${user.nombre}</td>
+                <td>${user.apellido}</td>
+                <td>${user.dni}</td>
+            </tr>
+            
+            `
+
+        });
+      
+
+        tablebody_users.innerHTML = contenido;
+        
         
     } catch (exc) {
         console.log(exc);
@@ -18,5 +39,7 @@ const listContacts= async()=>{
 window.addEventListener("load", async () => {
 
 await listContacts();
+
+$()
 
 });
